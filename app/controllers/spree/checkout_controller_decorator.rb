924382762aebf886abefcf835b0b9ed4311ff112
Ashton101 <ashton@platform45.com>
2014-11-06 10:32:02 +0200

    create seperate payment for success and fail transactions

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -35,13 +35,11 @@ Spree::CheckoutController.class_eval do
 
     if response[:successful] && response[:transaction_state] == 'SUCCESSFUL'
       flash.notice = (response[:display_message]).to_s + ' PAYMENT'
-      payment = create_pending_payment(Spree::PaymentMethod.find_by(name: 'PayU'))
-      payment.complete!
+      create_success_payment(Spree::PaymentMethod.find_by(name: 'PayU'))
       redirect_to order_url(@order)
     else
       flash.notice = response[:display_message]
-      payment = create_pending_payment(Spree::PaymentMethod.find_by(name: 'PayU'))
-      payment.failure!
+      create_failed_payment(Spree::PaymentMethod.find_by(name: 'PayU'))
     end
   end
 
@@ -59,7 +57,7 @@ Spree::CheckoutController.class_eval do
     @reference
   end
 
-  def create_pending_payment(payment_method)
+  def create_success_payment(payment_method)
     payment = @order.payments.build(
       payment_method_id: payment_method.id,
       amount: @order.total,
@@ -76,8 +74,22 @@ Spree::CheckoutController.class_eval do
       redirect_to checkout_state_path(@order.state) and return
     end
 
-    payment.pend!
-    payment
+    payment.complete!
+  end
+
+  def create_failed_payment(payment_method)
+    payment = @order.payments.build(
+      payment_method_id: payment_method.id,
+      amount: @order.total,
+      state: 'checkout'
+    )
+
+    unless payment.save
+      flash[:error] = payment.errors.full_messages.join("\n")
+      redirect_to checkout_state_path(@order.state) and return
+    end
+
+    payment.failure!
   end
 
   def payu_error(e = nil)
